84cf028ee1abd9c71294db3e48d815ce668da351
KNZ <86813245+knz2021@users.noreply.github.com>
2022-09-13 18:30:39 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,170 @@
+function clickDisplay1() {
+  const element = document.getElementById("text-button1");
+  element.innerHTML = "西暦前4026年　《アダム》の創造";
+}
+function clickDisplay2() {
+  const element = document.getElementById("text-button2");
+  element.innerHTML = "西暦前3096年　《アダム》の死";
+}
+function clickDisplay3() {
+  const element = document.getElementById("text-button3");
+  element.innerHTML = "西暦前3404年　義人《エノク》の誕生";
+}
+function clickDisplay4() {
+  const element = document.getElementById("text-button4");
+  element.innerHTML = "西暦前3039年　《エノク》は移される";
+}
+function clickDisplay5() {
+  const element = document.getElementById("text-button5");
+  element.innerHTML = "西暦前2970年　《ノア》の誕生";
+}
+function clickDisplay6() {
+  const element = document.getElementById("text-button6");
+  element.innerHTML = "西暦前2370年　《メトセラ》の死";
+}
+function clickDisplay7() {
+  const element = document.getElementById("text-button7");
+  element.innerHTML = "　　　　　　　ノアの《大洪水》";
+}
+function clickDisplay8() {
+  const element = document.getElementById("text-button8");
+  element.innerHTML = "西暦前2369年　《虹》の契約";
+}
+function clickDisplay9() {
+  const element = document.getElementById("text-button9");
+  element.innerHTML = "西暦前2018年　《アブラハム》の誕生";
+}
+function clickDisplay10() {
+  const element = document.getElementById("text-button10");
+  element.innerHTML = "西暦前1918年　《イサク》の誕生";
+}
+function clickDisplay11() {
+  const element = document.getElementById("text-button11");
+  element.innerHTML = "西暦前1878年　《イサク》と《リベカ》の結婚";
+}
+function clickDisplay12() {
+  const element = document.getElementById("text-button12");
+  element.innerHTML = "西暦前1843年　《アブラハム》の死";
+}
+function clickDisplay13() {
+  const element = document.getElementById("text-button13");
+  element.innerHTML = "西暦前1774年　《ヤコブ》と《ラケル》の結婚";
+}
+function clickDisplay14() {
+  const element = document.getElementById("text-button14");
+  element.innerHTML = "西暦前1767年　《ヨセフ》の誕生";
+}
+function clickDisplay15() {
+  const element = document.getElementById("text-button15");
+  element.innerHTML = "西暦前1761年　《ヤコブ》は《天使》と格闘";
+}
+function clickDisplay16() {
+  const element = document.getElementById("text-button16");
+  element.innerHTML = "西暦前1750年　《ヨセフ》は奴隷として売られる";
+}
+function clickDisplay17() {
+  const element = document.getElementById("text-button17");
+  element.innerHTML = "西暦前1737年　ヨセフは《エジプト》の総理大臣に";
+}
+function clickDisplay18() {
+  const element = document.getElementById("text-button18");
+  element.innerHTML = "西暦前1613年以前　《ヨブ》の試練";
+}
+function clickDisplay19() {
+  const element = document.getElementById("text-button19");
+  element.innerHTML = "西暦前1593年　《モーセ》の誕生";
+}
+function clickDisplay20() {
+  const element = document.getElementById("text-button20");
+  element.innerHTML = "西暦前1553年　モーセは《ミディアン》へ逃げる";
+}
+function clickDisplay21() {
+  const element = document.getElementById("text-button21");
+  element.innerHTML = "西暦前1513年 《紅海》を渡る";
+}
+function clickDisplay22() {
+  const element = document.getElementById("text-button22");
+  element.innerHTML = "西暦前1512年　《幕屋》の造営が完了";
+}
+function clickDisplay23() {
+  const element = document.getElementById("text-button23");
+  element.innerHTML = "西暦前1467年　《約束の地》の大半の征服を完了";
+}
+function clickDisplay24() {
+  const element = document.getElementById("text-button24");
+  element.innerHTML = "西暦前1117年　《サムエル》は《サウル》に油をそそぐ";
+}
+function clickDisplay25() {
+  const element = document.getElementById("text-button25");
+  element.innerHTML = "西暦前1107年　《ダビデ》が《ベツレヘム》で生まれる";
+}
+
+function clickDisplay27() {
+  const element = document.getElementById("text-button27");
+  element.innerHTML = "西暦前1070年　ダビデは全《イスラエル》の王となる";
+}
+function clickDisplay28() {
+  const element = document.getElementById("text-button28");
+  element.innerHTML = "西暦前1037年　《ソロモン》はイスラエルの王となる";
+}
+function clickDisplay29() {
+  const element = document.getElementById("text-button29");
+  element.innerHTML = "西暦前1027年　エルサレムの《神殿》が完成する";
+}
+function clickDisplay30() {
+  const element = document.getElementById("text-button30");
+  element.innerHTML = "西暦前997年　《レハベアム》はソロモンの跡を継ぐ";
+}
+function clickDisplay32() {
+  const element = document.getElementById("text-button32");
+  element.innerHTML =
+    "西暦前936年　《エフォシャファト》はアサの跡を継いでユダの王となる";
+}
+function clickDisplay33() {
+  const element = document.getElementById("text-button33");
+  element.innerHTML = "西暦前778年ごろ　《イザヤ》は預言を始める";
+}
+function clickDisplay34() {
+  const element = document.getElementById("text-button34");
+  element.innerHTML =
+    "西暦前745年　《ヒゼキヤ》はアハズの跡を継いでユダの王となる";
+}
+function clickDisplay35() {
+  const element = document.getElementById("text-button35");
+  element.innerHTML = "西暦前740年　アッシリアは《イスラエル》を征服";
+}
+function clickDisplay36() {
+  const element = document.getElementById("text-button36");
+  element.innerHTML =
+    "西暦前659年　《ヨシヤ》はアモンの跡を継いでユダの王となる";
+}
+function clickDisplay37() {
+  const element = document.getElementById("text-button37");
+  element.innerHTML = "西暦前647年　《エレミヤ》は預言者としての使命を受ける";
+}
+function clickDisplay38() {
+  const element = document.getElementById("text-button38");
+  element.innerHTML = "西暦前625年　《ネブカドネザル》はバビロンの王となる";
+}
+function clickDisplay39() {
+  const element = document.getElementById("text-button39");
+  element.innerHTML =
+    "西暦前617年　ユダヤ人の最初の捕らわれ人が《バビロン》へ連れて行かれる";
+}
+function clickDisplay40() {
+  const element = document.getElementById("text-button40");
+  element.innerHTML = "西暦前613年　《エゼキエル》は預言を始める";
+}
+function clickDisplay41() {
+  const element = document.getElementById("text-button41");
+  element.innerHTML = "西暦前607年　《エルサレム》は滅ぼされる";
+}
+function clickDisplay42() {
+  const element = document.getElementById("text-button42");
+  element.innerHTML = "西暦前539年　バビロンは《メディアペルシャ》の手に落ちる";
+}
+function clickDisplay43() {
+  const element = document.getElementById("text-button43");
+  element.innerHTML =
+    "西暦前455年　エルサレムの城壁は《ネヘミヤ》によって建て直される";
+}
